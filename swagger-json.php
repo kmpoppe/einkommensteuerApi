d201797da12622d2a250b26ae607395db69013a3
Kai Michael Poppe <kmpoppe@users.noreply.github.com>
2022-12-20 19:41:49 +0100

    Update swagger-json.php
    Beautify

--- swagger-json.php
+++ swagger-json.php
@@ -4,97 +4,99 @@ $validYearsESt = json_decode(file_get_contents("https://" . $_SERVER["HTTP_HOST"
 $validYearsSoli = json_decode(file_get_contents("https://" . $_SERVER["HTTP_HOST"]. "/methods/validYearsSoli", false, stream_context_create($fgcOptions)));
 ?>
 {
-	"swagger":"2.0",
-	"info":{
-		"version":"1.0.0",
-		"title":"Einkommensteuer API",
-		"license":{
-			"name":"MIT"
+	"swagger": "2.0",
+	"info": {
+		"version": "1.0.0",
+		"title": "Einkommensteuer API",
+		"license": {
+			"name": "MIT"
 		}
 	},
-	"host":"estapi.poppe.work",
-	"basePath":"/",
-	"schemes":[
+	"host": "estapi.poppe.work",
+	"basePath": "/",
+	"schemes": [
 		"https"
 	],
-	"consumes":[
+	"consumes": [
 		"application/json"
 	],
-	"produces":[
+	"produces": [
 		"application/json"
 	],
-	"paths":{
-		"/methods/calcESt":{
-			"post":{
-				"summary":"Berechnung der Jahreseinkommensteuer",
-				"parameters":[
+	"paths": {
+		"/methods/calcESt": {
+			"post": {
+				"summary": "Berechnung der Jahreseinkommensteuer",
+				"parameters": [
 					{
-						"in":"body",
-						"name":"body",
-						"description":"Angaben zur Berechnung",
-						"schema":{
-							"type":"object",
-							"required":[
+						"in": "body",
+						"name": "body",
+						"description": "Angaben zur Berechnung",
+						"schema": {
+							"type": "object",
+							"required": [
 								"year",
 								"splitting",
 								"zvE"
 							],
-							"properties":{
-								"year":{
-									"name":"year",
-									"type":"integer",
-									"description":"Jahr, für das die Berechnung erfolgen soll",
+							"properties": {
+								"year": {
+									"type": "integer",
+									"description": "Jahr, für das die Berechnung erfolgen soll",
 <?php
 echo str_repeat("\t", 9) . "\"minimum\":" . $validYearsESt->minimum . ",\n";
 echo str_repeat("\t", 9) . "\"maximum\":" . $validYearsESt->maximum . ",\n";
 ?>
-									"example":2018
+									"example": 2018
 								},
-								"splitting":{
-									"name":"splitting",
-									"type":"boolean",
-									"description":"Angabe, ob das Splittingverfahren für zusammenveranlagte steuerpflichtige Personen verwendet werden soll",
-									"example":true
+								"splitting": {
+									"type": "boolean",
+									"description": "Angabe, ob das Splittingverfahren für zusammenveranlagte steuerpflichtige Personen verwendet werden soll",
+									"example": true
 								},
-								"zvE":{
-									"name":"zvE",
-									"type":"integer",
-									"description":"Zu versteuerndes Einkommen",
-									"minimum":0,
-									"example":35678
+								"zvE": {
+									"type": "integer",
+									"description": "Zu versteuerndes Einkommen",
+									"minimum": 0,
+									"example": 35678
 								}
 							}
 						}
 					}
 				],
-				"responses":{
-					"200":{
-						"description":"Ergebnis der Berechnung",
-						"schema":{
-							"type":"object",
-							"properties":{
-								"calledMethod":{
-									"type":"string",
-									"description":"Rückgabe des Namens der aufgerufenen Methode zur Kontrolle",
-									"enum":"calcESt",
-									"example":"calcESt"
+				"responses": {
+					"200": {
+						"description": "Ergebnis der Berechnung",
+						"schema": {
+							"type": "object",
+							"properties": {
+								"calledMethod": {
+									"type": "string",
+									"description": "Rückgabe des Namens der aufgerufenen Methode zur Kontrolle",
+									"enum": [
+										"calcESt"
+									],
+									"example": "calcESt"
 								},
-								"result":{
-									"type":"string",
-									"description":"OK wenn die Berechnung erfolgreich war, Error wenn ein Fehler aufgetreten ist",
-									"enum": [ "OK", "Error" ],
+								"result": {
+									"type": "string",
+									"description": "OK wenn die Berechnung erfolgreich war, Error wenn ein Fehler aufgetreten ist",
+									"enum": [
+										"OK",
+										"Error"
+									],
 									"example": "OK"
 								},
-								"errors":{
-									"type":"string",
-									"description":"Liste der aufgetretenen Fehler",
-									"example":""
+								"errors": {
+									"type": "string",
+									"description": "Liste der aufgetretenen Fehler",
+									"example": ""
 								},
-								"value":{
-									"type":"integer",
-									"description":"Tarifliche Einkommensteuer, abgerundet auf den nächsten vollen Euro",
-									"minimum":0,
-									"example":3804
+								"value": {
+									"type": "integer",
+									"description": "Tarifliche Einkommensteuer, abgerundet auf den nächsten vollen Euro",
+									"minimum": 0,
+									"example": 3804
 								}
 							}
 						}
@@ -102,77 +104,79 @@ echo str_repeat("\t", 9) . "\"maximum\":" . $validYearsESt->maximum . ",\n";
 				}
 			}
 		},
-		"/methods/calcSoli":{
-			"post":{
-				"summary":"Berechnung des Solidaritätszuschlags",
-				"parameters":[
+		"/methods/calcSoli": {
+			"post": {
+				"summary": "Berechnung des Solidaritätszuschlags",
+				"parameters": [
 					{
-						"in":"body",
-						"name":"body",
-						"description":"Angaben zur Berechnung",
-						"schema":{
-							"type":"object",
-							"required":[
+						"in": "body",
+						"name": "body",
+						"description": "Angaben zur Berechnung",
+						"schema": {
+							"type": "object",
+							"required": [
 								"year",
 								"splitting",
 								"ESt"
 							],
-							"properties":{
-								"year":{
-									"name":"year",
-									"type":"integer",
-									"description":"Jahr, für das die Berechnung erfolgen soll",
+							"properties": {
+								"year": {
+									"type": "integer",
+									"description": "Jahr, für das die Berechnung erfolgen soll",
 <?php
 echo str_repeat("\t", 9) . "\"minimum\":" . $validYearsSoli->minimum . ",\n";
 echo str_repeat("\t", 9) . "\"maximum\":" . $validYearsSoli->maximum . ",\n";
 ?>
-									"example":2018
+									"example": 2018
 								},
-								"splitting":{
-									"name":"splitting",
-									"type":"boolean",
-									"description":"Angabe, ob das Splittingverfahren für zusammenveranlagte steuerpflichtige Personen verwendet werden soll",
-									"example":true
+								"splitting": {
+									"type": "boolean",
+									"description": "Angabe, ob das Splittingverfahren für zusammenveranlagte steuerpflichtige Personen verwendet werden soll",
+									"example": true
 								},
-								"zvE":{
-									"name":"ESt",
-									"type":"integer",
-									"description":"Tarifliche Einkommensteuer, auf die der Solidaritätszuschlag berechent werden soll",
-									"minimum":0,
-									"example":3804
+								"zvE": {
+									"type": "integer",
+									"description": "Tarifliche Einkommensteuer, auf die der Solidaritätszuschlag berechent werden soll",
+									"minimum": 0,
+									"example": 3804
 								}
 							}
 						}
 					}
 				],
-				"responses":{
-					"200":{
-						"description":"Ergebnis der Berechnung",
-						"schema":{
-							"type":"object",
-							"properties":{
-								"calledMethod":{
-									"type":"string",
-									"description":"Rückgabe des Namens der aufgerufenen Methode zur Kontrolle",
-									"enum":"calcESt",
-									"example":"calcESt"
+				"responses": {
+					"200": {
+						"description": "Ergebnis der Berechnung",
+						"schema": {
+							"type": "object",
+							"properties": {
+								"calledMethod": {
+									"type": "string",
+									"description": "Rückgabe des Namens der aufgerufenen Methode zur Kontrolle",
+									"enum": [
+										"calcSoli"
+									],
+									"example": "calcSoli"
 								},
-								"result":{
-									"type":"string",
-									"description":"OK wenn die Berechnung erfolgreich war, Error wenn ein Fehler aufgetreten ist",
-									"enum": [ "OK", "Error" ],
+								"result": {
+									"type": "string",
+									"description": "OK wenn die Berechnung erfolgreich war, Error wenn ein Fehler aufgetreten ist",
+									"enum": [
+										"OK",
+										"Error"
+									],
 									"example": "OK"
 								},
-								"errors":{
-									"type":"string",
-									"description":"Liste der aufgetretenen Fehler",
-									"example":""
+								"errors": {
+									"type": "string",
+									"description": "Liste der aufgetretenen Fehler",
+									"example": ""
 								},
-								"value":{
-									"type":"number",
-									"description":"Solidaritätszuschlag, abgerundet auf den nächsten Euro-Cent",
-									"minimum":0,
-									"example":209.22
+								"value": {
+									"type": "number",
+									"description": "Solidaritätszuschlag, abgerundet auf den nächsten Euro-Cent",
+									"minimum": 0,
+									"example": 209.22
 								}
 							}
 						}
@@ -180,24 +184,26 @@ echo str_repeat("\t", 9) . "\"maximum\":" . $validYearsSoli->maximum . ",\n";
 				}
 			}
 		},
-		"/methods/validYearsESt":{
-			"get":{
-				"summary":"Verfügbare Jahre für Berechnung der Einkommensteuer",
-				"responses":{
-					"200":{
-						"description":"Jahresbereich",
-						"schema":{
-							"type":"object",
-							"properties":{
-								"calledMethod":{
-									"type":"string",
-									"description":"Rückgabe des Namens der aufgerufenen Methode zur Kontrolle",
-									"enum":"validYearsESt",
-									"example":"validYearsESt"
+		"/methods/validYearsESt": {
+			"get": {
+				"summary": "Verfügbare Jahre für Berechnung der Einkommensteuer",
+				"responses": {
+					"200": {
+						"description": "Jahresbereich",
+						"schema": {
+							"type": "object",
+							"properties": {
+								"calledMethod": {
+									"type": "string",
+									"description": "Rückgabe des Namens der aufgerufenen Methode zur Kontrolle",
+									"enum": [
+										"validYearsESt"
+									],
+									"example": "validYearsESt"
 								},
-								"minimum":{
-									"type":"integer",
-									"description":"Erstes Jahr, für das eine Berechnung ausgeführt werden kann",
+								"minimum": {
+									"type": "integer",
+									"description": "Erstes Jahr, für das eine Berechnung ausgeführt werden kann",
 <?php
 echo str_repeat("\t", 9) . "\"example\":" . $validYearsESt->minimum . "\n";
 ?>
@@ -215,24 +221,26 @@ echo str_repeat("\t", 9) . "\"example\":" . $validYearsESt->maximum . "\n";
 				}
 			}
 		},
-		"/methods/validYearsSoli":{
-			"get":{
-				"summary":"Verfügbare Jahre für Berechnung des Solidaritätszuschlags",
-				"responses":{
-					"200":{
-						"description":"Jahresbereich",
-						"schema":{
-							"type":"object",
-							"properties":{
-								"calledMethod":{
-									"type":"string",
-									"description":"Rückgabe des Namens der aufgerufenen Methode zur Kontrolle",
-									"enum":"validYearsESt",
-									"example":"validYearsESt"
+		"/methods/validYearsSoli": {
+			"get": {
+				"summary": "Verfügbare Jahre für Berechnung des Solidaritätszuschlags",
+				"responses": {
+					"200": {
+						"description": "Jahresbereich",
+						"schema": {
+							"type": "object",
+							"properties": {
+								"calledMethod": {
+									"type": "string",
+									"description": "Rückgabe des Namens der aufgerufenen Methode zur Kontrolle",
+									"enum": [
+										"validYearsESt"
+									],
+									"example": "validYearsESt"
 								},
-								"minimum":{
-									"type":"integer",
-									"description":"Erstes Jahr, für das eine Berechnung ausgeführt werden kann",
+								"minimum": {
+									"type": "integer",
+									"description": "Erstes Jahr, für das eine Berechnung ausgeführt werden kann",
 <?php
 echo str_repeat("\t", 9) . "\"example\":" . $validYearsSoli->minimum . "\n";
 ?>
